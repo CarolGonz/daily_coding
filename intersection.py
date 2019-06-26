a3d176469892f9a7515b3ceb3c3bd1fe49c173cf
CarolGonz <52215378+CarolGonz@users.noreply.github.com>
2019-06-26 14:51:51 -0300

    Add files via upload
    intersection.py: defines functions of different methods intersection between two lists

---
+++ intersection.py
@@ -0,0 +1,13 @@
+
+class intersection_list:
+    
+    def intersection_1(lst1, lst2): 
+        lst3 = [value for value in lst1 if value in lst2] 
+        return lst3
+    
+    def intersection_2(lst1, lst2): 
+        return set(lista1).intersection(lista2)
+    
+    def intersection_len():
+        return len(set(lista1).intersection(lista2))
+    
